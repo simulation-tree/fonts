b27e56436121aa139e278d94cdca060b9cb0e4df
Phill <philldamaskin@gmail.com>
2024-09-25 19:13:29 -0400

    add vertex generation method

--- source/Font.cs
+++ source/Font.cs
@@ -2,7 +2,10 @@
 using Fonts.Components;
 using Simulation;
 using System;
+using System.Numerics;
 using Unmanaged;
+using static System.Net.Mime.MediaTypeNames;
+using Unmanaged.Collections;
 
 namespace Fonts
 {
@@ -31,7 +34,7 @@ namespace Fonts
 
         readonly World IEntity.World => entity.world;
         readonly uint IEntity.Value => entity.value;
-        readonly Definition IEntity.Definition => new([RuntimeType.Get<IsFont>()], [RuntimeType.Get<FontGlyph>()]);
+        readonly Definition IEntity.Definition => new Definition().AddComponentType<IsFont>().AddArrayType<FontGlyph>();
 
 #if NET
         [Obsolete("Default constructor not available", true)]
@@ -84,6 +87,65 @@ namespace Fonts
             buffer[length++] = '`';
             buffer[length++] = ')';
             return length;
+        }
+
+        public readonly Vector2 CalulcateSize(USpan<char> text, uint pixelSize)
+        {
+            USpan<Vector3> temp = stackalloc Vector3[(int)(text.Length * 4)];
+            return GenerateVertices(text, temp, pixelSize);
+        }
+
+        public readonly Vector2 GenerateVertices(USpan<char> text, USpan<Vector3> vertices, uint pixelSize)
+        {
+            uint lineHeight = LineHeight;
+            int penX = 0;
+            int penY = 0;
+            Vector2 maxPosition = default;
+            World world = entity.GetWorld();
+            USpan<FontGlyph> glyphs = entity.GetArray<FontGlyph>();
+            for (uint i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                rint glyphReference = glyphs[c].value;
+                uint glyphEntity = entity.GetReference(glyphReference);
+                IsGlyph glyph = world.GetComponent<IsGlyph>(glyphEntity);
+                if (c == '\n')
+                {
+                    penX = 0;
+                    penY -= (int)lineHeight;
+                    continue;
+                }
+
+                (int x, int y) glyphOffset = glyph.offset;
+                (int x, int y) glyphAdvance = glyph.advance;
+                (int x, int y) glyphSize = glyph.size;
+                (int x, int y) glyphBearing = glyph.bearing;
+                float glyphWidth = glyphSize.x;
+                float glyphHeight = glyphSize.y;
+                Vector2 origin = new(penX + (glyphOffset.x), penY + (glyphOffset.y));
+                origin.Y -= (glyphSize.y - glyphBearing.y);
+                Vector2 size = new(glyphWidth, glyphHeight);
+                origin /= 64f; //why is this divided by 64 again?
+                size /= 64f;
+                Vector2 first = origin;
+                Vector2 second = origin + new Vector2(size.X, 0);
+                Vector2 third = origin + new Vector2(size.X, size.Y);
+                Vector2 fourth = origin + new Vector2(0, size.Y);
+                penX += glyphAdvance.x;
+                //penY += advance.y / pixelSize;
+
+                vertices[(i * 4) + 0] = new Vector3(first, 0) / pixelSize;
+                vertices[(i * 4) + 1] = new Vector3(second, 0) / pixelSize;
+                vertices[(i * 4) + 2] = new Vector3(third, 0) / pixelSize;
+                vertices[(i * 4) + 3] = new Vector3(fourth, 0) / pixelSize;
+
+                maxPosition = Vector2.Max(maxPosition, first);
+                maxPosition = Vector2.Max(maxPosition, second);
+                maxPosition = Vector2.Max(maxPosition, third);
+                maxPosition = Vector2.Max(maxPosition, fourth);
+            }
+
+            return maxPosition;
         }
 
         public readonly override bool Equals(object? obj)
