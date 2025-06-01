6d4eca600ee9d5ed939ed92e7c25e8aed5663779
Phill <philldamaskin@gmail.com>
2025-06-01 12:26:35 -0400

    make font component not readonly

--- source/Components/IsFont.cs
+++ source/Components/IsFont.cs
@@ -1,24 +1,14 @@
 ﻿namespace Fonts.Components
 {
-    public readonly struct IsFont
+    public struct IsFont
     {
-        public readonly uint version;
-        public readonly uint pixelSize;
+        public uint version;
+        public uint pixelSize;
 
         public IsFont(uint version, uint pixelSize)
         {
             this.version = version;
             this.pixelSize = pixelSize;
-        }
-
-        public readonly IsFont IncrementVersion()
-        {
-            return new IsFont(version + 1, pixelSize);
-        }
-
-        public readonly IsFont IncrementVersion(uint pixelSize)
-        {
-            return new IsFont(version + 1, pixelSize);
         }
     }
 }
